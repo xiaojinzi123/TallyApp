cbb897a85d8d77cc30f0a33c9e7fe498b1e5a88c
JRobot <masterr_robot@163.com>
2023-06-03 11:03:14 +0800

    统计界面增加月账单报表

--- app-tally/module-statistical/src/main/java/com/xiaojinzi/tally/statistical/module/core/view/MonthlyStatisticalViews.kt
+++ app-tally/module-statistical/src/main/java/com/xiaojinzi/tally/statistical/module/core/view/MonthlyStatisticalViews.kt
@@ -3,12 +3,31 @@ package com.xiaojinzi.tally.statistical.module.core.view
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidthIn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ScrollableTabRow
+import androidx.compose.material.Tab
+import androidx.compose.material.TabPosition
 import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -28,7 +47,13 @@ import com.xiaojinzi.module.base.bean.toStringItemDTO
 import com.xiaojinzi.module.base.view.compose.AppTabView
 import com.xiaojinzi.module.base.view.compose.TabVO
 import com.xiaojinzi.support.ktx.nothing
-import com.xiaojinzi.tally.base.view.*
+import com.xiaojinzi.tally.base.support.tallyNumberFormat1
+import com.xiaojinzi.tally.base.view.BillDayListReportView
+import com.xiaojinzi.tally.base.view.CateGroupCostPercentGroupVO
+import com.xiaojinzi.tally.base.view.CateGroupCostPercentView
+import com.xiaojinzi.tally.base.view.PieChartVO
+import com.xiaojinzi.tally.base.view.PieChartView
+import com.xiaojinzi.tally.base.view.toPieChartItemVO
 import com.xiaojinzi.tally.statistical.R
 
 @Composable
@@ -111,6 +136,8 @@ fun MonthlyStatisticalView() {
     val selectYear by vm.monthlyStatisticalUseCase.selectYearObservableDTO.collectAsState(initial = -1)
     val selectIndex by vm.monthlyStatisticalUseCase.selectMonthObservableDTO.collectAsState(initial = 0)
     val monthCostTabSelectIndex by vm.monthCostTabSelectIndexObservableVO.collectAsState(initial = 0)
+    val monthReport by vm.monthlyStatisticalUseCase.selectMonthReportObservableDTO.collectAsState(
+        initial = Triple(0f, 0f, 0f))
     val tabIndicator = @Composable { tabPositions: List<TabPosition> ->
         MonthlyStatisticalTabIndicator(
             modifier = Modifier.tabIndicatorOffset(tabPositions[selectIndex])
@@ -173,6 +200,88 @@ fun MonthlyStatisticalView() {
                             style = MaterialTheme.typography.caption,
                         )
                     }
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        //月度总计
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 0.dp)
+                .verticalScroll(state = rememberScrollState())
+                .nothing()
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .clip(shape = MaterialTheme.shapes.small)
+                    .background(color = MaterialTheme.colors.primary)
+                    .padding(vertical = 12.dp)
+                    .nothing(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier
+                        .weight(weight = 1f, fill = true)
+                        .wrapContentHeight(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.res_str_monthly_spending),
+                        style = MaterialTheme.typography.body2.copy(
+                            color = MaterialTheme.colors.onPrimary
+                        )
+                    )
+                    Spacer(modifier = Modifier.height(height = 4.dp))
+                    Text(
+                        text = monthReport.first.tallyNumberFormat1(),
+                        style = MaterialTheme.typography.body1.copy(
+                            color = MaterialTheme.colors.onPrimary
+                        ),
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .weight(weight = 1f, fill = true)
+                        .wrapContentHeight(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.res_str_monthly_income),
+                        style = MaterialTheme.typography.body2.copy(
+                            color = MaterialTheme.colors.onPrimary
+                        )
+                    )
+                    Spacer(modifier = Modifier.height(height = 4.dp))
+                    Text(
+                        text = monthReport.second.tallyNumberFormat1(),
+                        style = MaterialTheme.typography.body1.copy(
+                            color = MaterialTheme.colors.onPrimary
+                        ),
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .weight(weight = 1f, fill = true)
+                        .wrapContentHeight(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.res_str_monthly_balance),
+                        style = MaterialTheme.typography.body2.copy(
+                            color = MaterialTheme.colors.onPrimary
+                        )
+                    )
+                    Spacer(modifier = Modifier.height(height = 4.dp))
+                    Text(
+                        text = monthReport.third.tallyNumberFormat1(),
+                        style = MaterialTheme.typography.body1.copy(
+                            color = MaterialTheme.colors.onPrimary
+                        ),
+                    )
                 }
             }
         }
